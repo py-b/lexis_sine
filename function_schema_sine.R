99626f6152c7294fa7a91b446e7aeecd9709ef86
Pierre-Yves Berrard <pierre.yves.berrard@gmail.com>
2018-03-16 14:16:16 +0100

    first commit

---
+++ function_schema_sine.R
@@ -0,0 +1,173 @@
+library(tibble)
+library(ggplot2)
+library(dplyr)
+
+
+# DATA --------------------------------------------------------------------
+
+annees_full <- 1994:2033
+
+couleurs <- setNames(
+  c("#666666", "#1B9E77", "#D95F02", "#7570B3", "#E7298A",
+    "#66A61E", "#A6761D", "#1F78B4", "#E6AB02", "#E31A1C"),
+  as.character(seq(1994, 2033, by = 4))
+)
+
+data_full <- 
+  tibble(
+    annee = annees_full,
+    age = rep(
+      c(0, 5, NA, 3),
+      length.out = max(annees_full) - min(annees_full) + 1
+    ),
+    cohorte = as.character(annee - age)
+  ) %>% 
+  filter(!is.na(age), cohorte != "1990")
+
+
+# GRAPH --------------------------------------------------------------
+
+schema_sine <- function(
+  annee1,
+  annee2,
+  cohorte_proj1, # première cohorte non commencée
+  cohorte_drop = NULL # pour masquer une cohorte sur le graphique
+) {
+  
+  if (missing(cohorte_proj1)) cohorte_proj1 <- 2100
+  
+  ## toutes années de toutes cohortes présentes dans années sélectionnées
+  cohortes <- 
+    data_full %>%
+    filter(annee %>% between(annee1, annee2)) %>%
+    `[[`("cohorte") %>%
+    unique()
+  data_graph <- data_full %>% filter(cohorte %in% cohortes)
+  
+  ## ajout points fictifs en 2100 pour prolongation des traits obliques
+  data_graph <-
+    bind_rows(
+      data_graph,
+      tibble(
+        cohorte = cohortes,
+        annee = 2100,
+        age = annee - as.double(cohorte)
+      )
+    ) %>%
+    mutate(projet = as.numeric(cohorte) >= cohorte_proj1)
+  
+  if (!is.null(cohorte_drop))
+    data_graph <-
+      data_graph %>%
+      filter(!cohorte %in% as.character(cohorte_drop)) 
+
+  ## plot
+
+  # initialisation
+  ggplot(
+    data = data_graph,
+    mapping = aes(x = annee)
+  ) +
+  # zoom sur années sélectionnées
+  coord_cartesian(
+    xlim = c(annee1 + 0.3, annee2 - 0.3),
+    ylim = c(-0.3, 7.1)
+  ) +
+  # barres verticales
+  geom_bar(
+    mapping = aes(weight = age, fill = cohorte),
+    position = "identity",
+    width = 0.05,
+    show.legend = FALSE
+  ) +
+  # traits obliques (cohorte en cours ou finie)
+  geom_line(
+    data = data_graph %>% filter(!projet),
+    mapping = aes(y = age, colour = cohorte),
+    linetype = "solid",
+    size = .3
+  ) +
+  # traits obliques (cohorte projet)
+  geom_line(
+    data = data_graph %>% filter(projet),
+    mapping = aes(y = age, colour = cohorte),
+    linetype = "dashed"
+  ) +
+  # texte "(projet)"
+  geom_text(
+    data = data_graph %>% filter(projet, age == 0),
+    mapping = aes(y = age - 0.4, colour = cohorte),
+    label = "(projet)",
+    size = 3
+  ) +
+  # interrogation entreprise (carré)
+  geom_point(
+    mapping = aes(y = age, colour = cohorte),
+    size = 5,
+    shape = 15
+  ) +
+  # interrogation entreprise (point interrogation)
+  geom_point(
+    mapping = aes(y = age),
+    size = 3,
+    shape = "?",
+    colour = "white"
+  ) +
+  # axe x
+  scale_x_continuous(
+    name = "",
+    breaks = annee1:annee2,
+    minor_breaks = NULL
+  ) +
+  # axe y
+  scale_y_continuous(
+    name = "",
+    breaks = c(0, 3, 5),
+    minor_breaks = 0:7,
+    labels = c("Année\nde la\ncréation", "3 ans", "5 ans")
+  ) +
+  # couleur séries (barres verticales)
+  scale_fill_manual(
+    values = couleurs[cohortes]
+  ) +
+  # couleur séries (autres couches)
+  scale_colour_manual(
+    values = couleurs[cohortes],
+    name = "Cohorte"
+  ) +
+  # cosmétique axes et légende
+  theme(
+    axis.text.x = element_text(angle = 50, hjust = 1, vjust = 0.5),
+    legend.key.size = unit(0.6, "cm"),
+    legend.key.width = unit(1, "cm"),
+    axis.line = element_line(
+      colour = "gray19",
+      arrow = arrow(angle = 30, unit(0.07, "inches"))
+    )
+  ) +
+  # note de pied de page
+  labs(
+    caption = "? = interrogation des entreprises"
+  )
+
+}
+
+# APPEL -------------------------------------------------------------------
+
+# g <- 
+# schema_sine(
+#   2002,
+#   2020,
+#   cohorte_proj1 = 2018
+# )
+
+# SAVE --------------------------------------------------------------------
+
+# ggsave("lexis_sine_full.png", g, width = 7.5, height = 3.5)
+# ggsave("lexis_sine_100dpi.png", g, width = 7.5, height = 3.5, dpi = 100)
+# ggsave("lexis_sine_150dpi.png", g, width = 7.5, height = 3.5, dpi = 150)
+# ggsave("lexis_sine_200dpi.png", g, width = 7.5, height = 3.5, dpi = 200)
+# ggsave("lexis_sine_300dpi.png", g, width = 7.5, height = 3.5, dpi = 300)
+
+# ggsave("lexis_auto_300dpi.png", g, width = 6, height = 3.5, dpi = 300)
+# ggsave("lexis_auto_150dpi.png", g, width = 6, height = 3.5, dpi = 150)
---
+++ shiny_graph.R
@@ -0,0 +1,142 @@
+# LOAD --------------------------------------------------------------------
+
+library(shiny)
+library(shinyjs)
+jscode <- "shinyjs.closeWindow = function() { window.close(); }"
+options(encoding = "UTF-8")
+source("function_schema_sine.R")
+
+
+# UI ----------------------------------------------------------------------
+
+
+ui <- fluidPage(
+
+  titlePanel("Générateur calendrier Sine"),
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      
+      useShinyjs(),
+    
+      extendShinyjs(
+        text = jscode,
+        functions = c("closeWindow")
+      ),
+      
+      sliderInput(
+        inputId = "bornes",
+        label = "Période",
+        min = 1994,
+        max = 2030,
+        value = c(1998, 2020),
+        sep = ""
+      ),
+      
+      selectInput(
+        inputId = "proj",
+        label = "Première cohorte pas encore enquêtée",
+        choices = "<aucune>"
+      ),
+      
+      selectInput(
+        inputId = "drop",
+        label = "Masquer une cohorte",
+        choices = "<aucune>"
+      )
+      
+    ),
+    
+    mainPanel(
+      
+      h3("Prévisualisation"),
+      
+      plotOutput(
+        outputId = "graphique",
+        width = "80%"
+      ),
+      
+      downloadButton(
+        outputId = "sauv_img",
+        label = "Export image"
+      ),
+      
+      actionButton(
+        inputId = "bQuit",
+        label = "Quitter"
+      )
+      
+    )
+    
+  )
+
+)  
+
+
+# SERVER ------------------------------------------------------------------
+
+
+server <- function(input, output, session) {
+  
+  cohortes_graph <- reactive(
+    data_full %>%
+    filter(annee %>% between(input$bornes[1], input$bornes[2])) %>%
+    `[[`("cohorte") %>%
+    unique %>%
+    sort %>%
+    c("<aucune>", .)
+  )
+  
+  graph <- reactive({
+    drop <- if(input$drop == "<aucune>") NULL else as.integer(input$drop)
+    proj <- if(input$proj == "<aucune>") 2100 else as.integer(input$proj)
+    schema_sine(
+      as.integer(input$bornes[1]),
+      as.integer(input$bornes[2]),
+      cohorte_proj1 = proj,
+      cohorte_drop = drop
+    )
+  })
+  
+  observe({
+    updateSelectInput(
+      session,
+      inputId = "proj",
+      choices = cohortes_graph(),
+      selected = "<aucune>"
+    )
+  })
+  
+  observe({
+    updateSelectInput(
+      session,
+      inputId = "drop",
+      choices = c("<aucune>", cohortes_graph()),
+      selected = "<aucune>"
+    )
+  })
+  
+  output$graphique <- renderPlot(
+    graph()
+  )
+  
+  output$sauv_img <- downloadHandler(
+    filename = function() {
+      sprintf("schema_sine_%s-%s.png", input$bornes[1], input$bornes[2])
+    },
+    content = function(file) ggsave(file, graph(), width = 7.5, height = 3.5)
+  )
+  
+  observeEvent(input$bQuit, {
+    js$closeWindow()
+    stopApp()
+  })
+  
+}
+
+
+# RUN APP -----------------------------------------------------------------
+
+
+shinyApp(ui, server)
